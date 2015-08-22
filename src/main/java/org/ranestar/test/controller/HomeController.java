d82f754f999ea50c275b0e63ea38c17ea2e5a68a
z101043 <z101043@naver.com>
2015-08-22 12:40:47 +0900

    변경

--- src/main/java/org/ranestar/test/controller/HomeController.java
+++ src/main/java/org/ranestar/test/controller/HomeController.java
@@ -4,10 +4,8 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import org.ranestar.test.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +18,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	@Autowired UserService userService;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
--- src/test/java/org/ranestar/test/daotest/UserDaoTest.java
+++ src/test/java/org/ranestar/test/daotest/UserDaoTest.java
@@ -11,8 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/**/*-context.xml" , 
-"classpath:context/**/applicationContext*.xml"})
+@ContextConfiguration(locations={"classpath:context/**/applicationContext*.xml"})
 public class UserDaoTest {
 	
 	private static final Logger logger = LoggerFactory.getLogger(UserDaoTest.class);
